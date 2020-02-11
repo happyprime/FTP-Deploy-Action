df7dcf846b7b5c459612ca5c34f56ee1bfdad0d2
Jeremy Felt <jeremy.felt@gmail.com>
2020-02-10 22:27:58 -0800

    Add trailing slashes to excluded directories

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,7 @@ echo "Connecting"
 echo "Uploading files..."
 echo "set ftp:ssl-allow no; mirror -X node_modules -X vendor -X .git --verbose --delete --delete-excluded -R $LOCAL_DIR $REMOTE_DIR; quit"
 
-lftp $FTP_METHOD://$FTP_SERVER:$FTP_PORT -u $FTP_USERNAME,$FTP_PASSWORD -e "set ftp:ssl-allow no; mirror -X node_modules -X vendor -X .git --verbose --delete --delete-excluded -R $LOCAL_DIR $REMOTE_DIR; quit"
+lftp $FTP_METHOD://$FTP_SERVER:$FTP_PORT -u $FTP_USERNAME,$FTP_PASSWORD -e "set ftp:ssl-allow no; mirror -x node_modules/ -X vendor/ -X .git/ --verbose --delete --delete-excluded -R $LOCAL_DIR $REMOTE_DIR; quit"
 
 echo "FTP Deploy Complete"
 exit 0
